2a8bb8f98ee466820c029ffd76508d6a0c17b760
matmunk <matmunk@gmail.com>
2013-08-29 02:53:33 +0200

    Updated example.

--- examples/Alarms/Alarms.ino
+++ examples/Alarms/Alarms.ino
@@ -1,3 +1,26 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 Mathias Munk Hansen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include <OneWire.h>
 #include <DS18B20.h>
 
@@ -10,40 +33,37 @@
 // 1-Wire devices connected to digital pin 2 on the Arduino.
 DS18B20 ds(2);
 
-// Array for holding the 64 bit address of the current device.
-uint8_t address[8];
-
 void setup() {
   Serial.begin(9600);
   
   // Set low and high alarms of all devices.
-  while(ds.getNextDevice(address))
+  while(ds.selectNext())
   {
-    ds.setAlarms(LOW_ALARM, HIGH_ALARM, address);
+    ds.setAlarms(LOW_ALARM, HIGH_ALARM);
   }
 }
 
 void loop()
 {
   // Tell every device to start a temperature conversion.
-  ds.startConversion();
+  ds.doConversion();
   
-  // Print alarm values and current temperature for every device with an active alarm.
-  while(ds.getNextAlarm(address))
+  // Print alarm values and current temperature for every device with an active alarm condition.
+  while(ds.selectNextAlarm())
   {
     // Print value of low alarm.
     Serial.print("Alarm Low: ");
-    Serial.print(ds.getAlarmLow(address));
+    Serial.print(ds.getAlarmLow());
     Serial.println(" C");
     
     // Print value of high alarm.
     Serial.print("Alarm High: ");
-    Serial.print(ds.getAlarmHigh(address));
+    Serial.print(ds.getAlarmHigh());
     Serial.println(" C");
     
     // Print current temperature to verify that it is still either < LOW_ALARM or > HIGH_ALARM.
     Serial.print("Temperature: ");
-    Serial.print(ds.getTempC(address));
+    Serial.print(ds.getTempC());
     Serial.println(" C\n");
   }
   
